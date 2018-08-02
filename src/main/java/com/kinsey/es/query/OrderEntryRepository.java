edb0fe004549fb073f0b829839514f6c237570ff
kinsey-jian <1476684295@qq.com>
2018-08-02 22:50:45 +0800

    update

--- src/main/java/com/kinsey/es/query/OrderEntryRepository.java
+++ src/main/java/com/kinsey/es/query/OrderEntryRepository.java
@@ -1,8 +1,9 @@
 package com.kinsey.es.query;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderEntryRepository extends MongoRepository<OrderEntry, String> {
+public interface OrderEntryRepository extends MongoRepository<OrderEntry, String>, QuerydslPredicateExecutor<OrderEntry> {
 }
